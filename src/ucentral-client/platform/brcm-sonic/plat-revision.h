6f6bd4dfd058bf5615c800c7fc50260e33bb5ae8
Oleksandr Mazur <oleksandr.mazur@plvision.eu>
2024-03-29 16:59:20 +0200

    Update build version to 2.2 b2
    Signed-off-by: Oleksandr Mazur <oleksandr.mazur@plvision.eu>
    Change-Id: I1091f1f34d38f197bdc272adab018a2ec68b5bbf

--- src/ucentral-client/platform/brcm-sonic/plat-revision.h
+++ src/ucentral-client/platform/brcm-sonic/plat-revision.h
@@ -4,8 +4,8 @@
 #define XSTR(x) STR(x)
 #define STR(x) #x
 
-#define PLATFORM_REL_NUM 1.6
-#define PLATFORM_BUILD_NUM 5
+#define PLATFORM_REL_NUM 2.2
+#define PLATFORM_BUILD_NUM 2
 
 #ifndef PLATFORM_REVISION
 #define PLATFORM_REVISION "Rel " XSTR(PLATFORM_REL_NUM) " build " XSTR(PLATFORM_BUILD_NUM)
