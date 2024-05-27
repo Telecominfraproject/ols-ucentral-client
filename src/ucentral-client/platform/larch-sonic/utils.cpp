7b20f06aef663847f8e8bcb0c73ad6d5af461732
Viacheslav Holovetskyi <vholovetskyi@larch-networks.com>
2024-05-27 18:25:01 +0300

    Implement key-value parsing for YANG paths

--- src/ucentral-client/platform/larch-sonic/utils.cpp
+++ src/ucentral-client/platform/larch-sonic/utils.cpp
@@ -6,6 +6,7 @@
 
 #include <grpcpp/grpcpp.h>
 
+#include <map>
 #include <stdexcept>
 #include <string>
 #include <utility> // std::move
@@ -28,25 +29,133 @@ bool verify_response(const httplib::Result &result, bool expect_ok)
 	return true;
 }
 
+std::map<std::string, std::string> parse_kv(const std::string &kv_str)
+{
+	enum class parse_state { open_bracket, close_bracket, key, value };
+
+	parse_state state = parse_state::close_bracket;
+	std::map<std::string, std::string> kv;
+
+	std::string key_buf, value_buf;
+
+	for (const char c : kv_str)
+	{
+		switch (c)
+		{
+			case '[':
+				if (state != parse_state::close_bracket)
+					throw std::runtime_error{
+					    "Unexpected opening bracket"};
+
+				state = parse_state::open_bracket;
+				break;
+
+			case ']':
+				if (state != parse_state::value)
+					throw std::runtime_error{
+					    "Unexpected closing bracket"};
+
+				state = parse_state::close_bracket;
+
+				if (key_buf.empty())
+					throw std::runtime_error{"Empty key"};
+
+				if (value_buf.empty())
+					throw std::runtime_error{"Empty value"};
+
+				kv.emplace(
+				    std::move(key_buf),
+				    std::move(value_buf));
+				key_buf.clear();
+				value_buf.clear();
+
+				break;
+
+			case '=':
+				if (state != parse_state::key)
+					throw std::runtime_error{
+					    "Unexpected equals sign"};
+
+				state = parse_state::value;
+				break;
+
+			default:
+			{
+				if (state == parse_state::open_bracket)
+					state = parse_state::key;
+
+				if (state == parse_state::key)
+					key_buf.push_back(c);
+				else if (state == parse_state::value)
+					value_buf.push_back(c);
+				else
+					throw std::runtime_error{
+					    "Unexpected character '"
+					    + std::string{c} + "'"};
+
+				break;
+			}
+		}
+	}
+
+	if (state != parse_state::close_bracket)
+		throw std::runtime_error{"Couldn't find closing bracket"};
+
+	return kv;
+}
+
 void convert_yang_path_to_proto(std::string yang_path, gnmi::Path *proto_path)
 {
+	struct path_element {
+		std::string name;
+		std::map<std::string, std::string> kv;
+	};
+
 	std::string::size_type pos{};
-	std::vector<std::string> elements;
+	std::vector<path_element> elements;
 
 	while ((pos = yang_path.find('/')) != std::string::npos)
 	{
-		std::string elem{yang_path.substr(0, pos)};
+		std::string elem_str{yang_path.substr(0, pos)};
 
-		if (!elem.empty())
-			elements.push_back(std::move(elem));
+		if (!elem_str.empty())
+		{
+			const auto open_bracket_pos = elem_str.find('[');
+
+			if (open_bracket_pos == std::string::npos)
+			{
+				elements.push_back({std::move(elem_str), {}});
+			}
+			else
+			{
+				// Parse the key-value part of YANG path
+				// (e.g. [Vlan=100][SomeKey=SomeValue]...)
+				try
+				{
+					elements.push_back(
+					    {elem_str.substr(
+						 0,
+						 open_bracket_pos),
+					     parse_kv(elem_str.substr(
+						 open_bracket_pos))});
+				}
+				catch (const std::runtime_error &ex)
+				{
+					using namespace std::string_literals;
+					throw std::runtime_error{
+					    "Failed to parse key-value part of YANG path: "s
+					    + ex.what()};
+				}
+			}
+		}
 
 		yang_path.erase(0, pos + 1);
 	}
 
 	// Add the last part of split string
-	elements.push_back(yang_path);
+	elements.push_back({std::move(yang_path), {}});
 
-	std::string &first_element = elements[0];
+	std::string &first_element = elements[0].name;
 	const auto colon_pos = first_element.find(':');
 
 	if (colon_pos != std::string::npos)
@@ -58,7 +167,15 @@ void convert_yang_path_to_proto(std::string yang_path, gnmi::Path *proto_path)
 	for (const auto &elem : elements)
 	{
 		gnmi::PathElem *path_elem = proto_path->add_elem();
-		path_elem->set_name(elem);
+		path_elem->set_name(elem.name);
+
+		if (!elem.kv.empty())
+		{
+			auto path_kv = path_elem->mutable_key();
+
+			for (const auto &[key, value] : elem.kv)
+				(*path_kv)[key] = value;
+		}
 	}
 }
 
